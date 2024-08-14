3566257093507c75a4de41aed51044ea0972dbec
Gedion Abera <gedionable2@gmail.com>
2024-08-14 23:44:43 +0300

    fourth commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,17 @@
 function App() {
-  return <div className='text-red-500 bg-blue-400 p-2'>Hellow from the home page</div>;
+  return 
+  <div className='flex'>
+			<Sidebar />
+			<div className='max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1'>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+					<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
+					<Route path='/explore' element={authUser ? <ExplorePage /> : <Navigate to={"/login"} />} />
+					<Route path='/likes' element={authUser ? <LikesPage /> : <Navigate to={"/login"} />} />
+				</Routes>
+				<Toaster />
+			</div>
+		</div>
 }
 export default App;
