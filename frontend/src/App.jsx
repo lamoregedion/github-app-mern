8f02b4e328d93eec9d5ac22426b801adf63514f2
Gedion Abera <gedionable2@gmail.com>
2024-08-14 23:25:37 +0300

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,4 @@
-function App() { 
-
-  return (
-    <div>
-      Hellow from the home page
-    </div>
-  )
+function App() {
+  return <div className='text-red-500 bg-blue-400 p-2'>Hellow from the home page</div>;
 }
-
-export default App
+export default App;
